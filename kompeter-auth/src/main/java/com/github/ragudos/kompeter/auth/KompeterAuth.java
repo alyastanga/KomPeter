bb3538844e07b5c2e14e30c7b71f19fb6c989d4e
tip-aaron <qaamragudos@tip.edu.ph>
2025-10-08 01:45:57 +0800

    chore: rename groupId
    rename groupId to com.github.ragudos
    turn https schemas to http

---
+++ kompeter-auth/src/main/java/com/github/ragudos/kompeter/auth/KompeterAuth.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.github.ragudos.kompeter.auth;
+
+/**
+ *
+ * @author hinode
+ */
+public class KompeterAuth {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
