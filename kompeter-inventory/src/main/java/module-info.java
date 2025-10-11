0db48a7c4a72ed2ddc540a2663ba27a0b42e2c56
familyaccount <familyaccount@localhost>
2025-10-11 10:44:15 +0800

    refactor: Inventory mod-info

--- kompeter-inventory/src/main/java/module-info.java
+++ kompeter-inventory/src/main/java/module-info.java
@@ -1,1 +1,8 @@
-module kompeter.inventory {}
+module kompeter.inventory {
+    requires kompeter.database;
+    requires kompeter.utilities;
+    requires com.miglayout.swing;
+    requires java.desktop;
+    
+    exports com.github.ragudos.kompeter.inventory;
+}
