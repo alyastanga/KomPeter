b4a87f358e1977948969725711b931566f579663
tip-aaron <qaamragudos@tip.edu.ph>
2025-10-11 12:06:53 +0800

    refactor utilities
    - change directory paths to be appropriate for specific operating
      systems.
    - add comparisons for char[] in CharUtils

--- kompeter-utilities/src/test/java/com/github/ragudos/kompeter/utilities/TestCharUtils.java
+++ kompeter-utilities/src/test/java/com/github/ragudos/kompeter/utilities/TestCharUtils.java
@@ -10,11 +10,10 @@ public class TestCharUtils {
     @Test
     @DisplayName("Test byte[] to char[]")
     void testByteArrToCharArr() {
-        byte[] asciiBytes =
-                new byte[] {
-                    65, 66, 67, 68, 69, 70, 71, 72, 73, 74 // ASCII for 'A' to 'J'
-                };
-        char[] charBytes = new char[] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+        byte[] asciiBytes = new byte[] {
+                65, 66, 67, 68, 69, 70, 71, 72, 73, 74 // ASCII for 'A' to 'J'
+        };
+        char[] charBytes = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
 
         assertTrue(Arrays.equals(CharUtils.byteArrayToCharArray(asciiBytes), charBytes));
     }
@@ -22,11 +21,10 @@ public class TestCharUtils {
     @Test
     @DisplayName("Test char[] to byte[]")
     void testCharArrToByteArr() {
-        byte[] asciiBytes =
-                new byte[] {
-                    65, 66, 67, 68, 69, 70, 71, 72, 73, 74 // ASCII for 'A' to 'J'
-                };
-        char[] charBytes = new char[] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+        byte[] asciiBytes = new byte[] {
+                65, 66, 67, 68, 69, 70, 71, 72, 73, 74 // ASCII for 'A' to 'J'
+        };
+        char[] charBytes = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
 
         assertTrue(Arrays.equals(CharUtils.charArrayToByteArray(charBytes), asciiBytes));
     }
@@ -34,12 +32,13 @@ public class TestCharUtils {
     @Test
     @DisplayName("Test [] comparison")
     void testComparison() {
-        byte[] asciiBytes =
-                new byte[] {
-                    65, 66, 67, 68, 69, 70, 71, 72, 73, 74 // ASCII for 'A' to 'J'
-                };
-        char[] charBytes = new char[] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+        byte[] asciiBytes = new byte[] {
+                65, 66, 67, 68, 69, 70, 71, 72, 73, 74 // ASCII for 'A' to 'J'
+        };
+        char[] charBytes = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+        char[] charBytes2 = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
 
         assertTrue(CharUtils.constantTimeEquals(asciiBytes, CharUtils.charArrayToByteArray(charBytes)));
+        assertTrue(CharUtils.constantTimeEquals(charBytes, charBytes2));
     }
 }
