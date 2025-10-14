d2bff481402e1a1aa4a4d35e432b3cdb64801bb8
tip-aaron <qaamragudos@tip.edu.ph>
2025-10-15 01:01:10 +0800

    add button factory

---
+++ kompeter-app-desktop/src/main/java/com/github/ragudos/kompeter/app/desktop/components/factory/ButtonFactory.java
@@ -0,0 +1,22 @@
+package com.github.ragudos.kompeter.app.desktop.components.factory;
+
+import com.formdev.flatlaf.FlatClientProperties;
+import com.github.ragudos.kompeter.app.desktop.assets.AssetManager;
+import javax.swing.JButton;
+
+import org.jetbrains.annotations.NotNull;
+
+public final class ButtonFactory {
+    public static JButton createButton(
+            @NotNull String text, @NotNull String iconName, @NotNull String actionCommand, @NotNull String styleClass) {
+        JButton b = new JButton(text);
+
+        b.setIconTextGap(12);
+        b.setIcon(AssetManager.getOrLoadIcon("icons/" + iconName));
+        b.setActionCommand(actionCommand);
+        b.putClientProperty(FlatClientProperties.STYLE_CLASS, styleClass);
+        b.setHorizontalAlignment(JButton.LEFT);
+
+        return b;
+    }
+}
