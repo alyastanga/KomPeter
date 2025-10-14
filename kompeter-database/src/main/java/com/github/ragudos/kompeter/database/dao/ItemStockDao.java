26df2484bafd8e251fce6f0efd7112a1cac8c696
Hanzm10 <qhlmmapua@tip.edu.ph>
2025-10-14 17:51:45 +0800

    refactor(database): added new method implementation for ItemStockDao, PurchaseItemStockDao, PurchasePaymentDao, SalesItemStockDao, SalesPaymentDao. And added new ProfitMetaDataDao for profit methods

--- kompeter-database/src/main/java/com/github/ragudos/kompeter/database/dao/ItemStockDao.java
+++ kompeter-database/src/main/java/com/github/ragudos/kompeter/database/dao/ItemStockDao.java
@@ -1,3 +1,43 @@
 package com.github.ragudos.kompeter.database.dao;
 
-public interface ItemStockDao {}
+import com.github.ragudos.kompeter.database.dto.PurchaseItemStockDto;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.List;
+
+public interface ItemStockDao {
+
+    public List<PurchaseItemStockDto> getInventoryCount() throws SQLException;
+
+    public List<PurchaseItemStockDto> getInventoryCount(Timestamp from) throws SQLException;
+
+    public List<PurchaseItemStockDto> getInventoryCount(Timestamp from, Timestamp to) throws SQLException;
+
+    
+    public List<PurchaseItemStockDto> getInventoryValue() throws SQLException;
+
+    public List<PurchaseItemStockDto> getInventoryValue(Timestamp from) throws SQLException;
+
+    public List<PurchaseItemStockDto> getInventoryValue(Timestamp from, Timestamp to) throws SQLException;
+
+    
+    public List<PurchaseItemStockDto> getOnHandUnit();
+
+    public List<PurchaseItemStockDto> getOnHandUnit(Timestamp from);
+
+    public List<PurchaseItemStockDto> getOnHandUnit(Timestamp from, Timestamp to);
+
+    
+    public List<PurchaseItemStockDto> getLowStockItems();
+
+    public List<PurchaseItemStockDto> getLowStockItems(Timestamp from);
+
+    public List<PurchaseItemStockDto> getLowStockItems(Timestamp from, Timestamp to);
+
+    
+    public List<PurchaseItemStockDto> getOldItems();
+
+    public List<PurchaseItemStockDto> getOldItems(Timestamp from);
+
+    public List<PurchaseItemStockDto> getOldItems(Timestamp from, Timestamp to);
+}
--- kompeter-database/src/main/java/com/github/ragudos/kompeter/database/dao/PurchaseItemStockDao.java
+++ kompeter-database/src/main/java/com/github/ragudos/kompeter/database/dao/PurchaseItemStockDao.java
@@ -1,3 +1,12 @@
 package com.github.ragudos.kompeter.database.dao;
 
-public interface PurchaseItemStockDao {}
+import com.github.ragudos.kompeter.database.dto.PurchaseItemStockDto;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.List;
+
+public interface PurchaseItemStockDao {
+    public List<PurchaseItemStockDto> getPurchaseStock();
+    public List<PurchaseItemStockDto> getPurchaseStock(Timestamp from);
+    public List<PurchaseItemStockDto> getPurchaseStock(Timestamp from, Timestamp to);
+}
