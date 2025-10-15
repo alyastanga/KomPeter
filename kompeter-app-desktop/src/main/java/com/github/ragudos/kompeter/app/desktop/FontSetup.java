0fbe487cf6d9b0c27de4133618393cc433ab91e2
tip-aaron <qaamragudos@tip.edu.ph>
2025-10-15 10:16:13 +0800

    move font registration

---
+++ kompeter-app-desktop/src/main/java/com/github/ragudos/kompeter/app/desktop/FontSetup.java
@@ -0,0 +1,39 @@
+package com.github.ragudos.kompeter.app.desktop;
+
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.io.InputStream;
+import java.util.logging.Logger;
+
+import com.github.ragudos.kompeter.app.desktop.assets.AssetManager;
+import com.github.ragudos.kompeter.utilities.logger.KompeterLogger;
+
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.Resource;
+import io.github.classgraph.ScanResult;
+
+public final class FontSetup {
+    private static final Logger LOGGER = KompeterLogger.getLogger(FontSetup.class);
+
+    public static final void setup() {
+        try (ScanResult scanResult = new ClassGraph()
+                .acceptModules(AssetManager.class.getModule().toString())
+                .acceptPackages(AssetManager.class.getPackageName())
+                .acceptPaths("fonts")
+                .scan()) {
+            for (Resource res : scanResult.getResourcesWithExtension("ttf")) {
+                try (InputStream is = res.open()) {
+                    Font font = Font.createFont(Font.TRUETYPE_FONT, is);
+                    GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+                    ge.registerFont(font);
+
+                    LOGGER.info("Successfully registered font: " + font.getFamily());
+                } catch (Exception e) {
+                    LOGGER.log(Level.SEVERE, "Failed to register font", e);
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Failed to scan for fonts", e);
+        }
+    }
+}
