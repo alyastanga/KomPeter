ea7f90c06c8be4cb9f1c301e9f6412710c253cfc
familyaccount <familyaccount@localhost>
2025-10-19 17:59:00 +0800

    refactor: sql queries

--- kompeter-database/src/main/resources/com/github/ragudos/kompeter/database/sql/sqlite/select/items/select_item_stock_by_id.sql
+++ kompeter-database/src/main/resources/com/github/ragudos/kompeter/database/sql/sqlite/select/items/select_item_stock_by_id.sql
@@ -1,1 +1,13 @@
-SELECT * FROM item_stock WHERE _item_stock_id = ?;
+SELECT 
+    i._item_stock_id,
+    i._item_id,
+    i._item_brand_id,
+    i._created_at,
+    i.unit_price_php,
+    SUM(issl.quantity) AS quantity,
+    i.minimum_quantity
+FROM item_stocks AS i
+INNER JOIN item_stock_storage_locations AS issl ON i._item_stock_id = issl._item_stock_id
+WHERE i._item_stock_id = ?;
+
+
